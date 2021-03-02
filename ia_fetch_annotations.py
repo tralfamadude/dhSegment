8242beed345db6b20a68570f13dfd9785f74d700
Paul Baclace <peb@pebmac4.localdomain>
2021-03-01 19:44:56 -0800

    start of impl for fetch annotations

--- ia_fetch_annotations.py
+++ ia_fetch_annotations.py
@@ -10,3 +10,19 @@ r = requests.get('http://localhost:8080/api/v1/projects', auth=("peb", pw))
 if r.status_code != 200:
     print("problem with GET")
 r.encoding = 'utf-8'
+project_dict = r.json()
+project_list = project_dict["results"]
+for project in project_list:
+    project_name = project["name"]
+    task_list = project["tasks"]
+    for task in task_list:
+        task_url = task["url"]
+        image_count = task["size"]
+        annotation_url = task_url + "/annotations?format=COCO%201.0"
+        r_annotation = requests.get(annotation_url, auth=(user, pw))
+        if r_annotation.status_code != 202:
+          print("error: r_annotation status is not 202")
+        r_annotation2 = requests.get(annotation_url, auth=(user, pw))
+        if r_annotation2.status_code != 201:
+          print("error: r_annotation2 status is not 201")
+        r_annotation3 = requests.get(annotation_url + "&action=download", auth=(user, pw))
