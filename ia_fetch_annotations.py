f14a18786c1455bbbe0cf1bbfbcd826cbe111c43
Paul Baclace <peb@pebmac4.localdomain>
2021-03-02 19:38:52 -0800

    zipfile wrangling

--- ia_fetch_annotations.py
+++ ia_fetch_annotations.py
@@ -2,6 +2,7 @@ import requests
 from getpass import getpass
 import json
 import zipfile
+import io
 
 user = "peb"
 pw = '******'
@@ -21,8 +22,14 @@ for project in project_list:
         annotation_url = task_url + "/annotations?format=COCO%201.0"
         r_annotation = requests.get(annotation_url, auth=(user, pw))
         if r_annotation.status_code != 202:
-          print("error: r_annotation status is not 202")
+            print("error: r_annotation status is not 202")
         r_annotation2 = requests.get(annotation_url, auth=(user, pw))
         if r_annotation2.status_code != 201:
-          print("error: r_annotation2 status is not 201")
+            print("error: r_annotation2 status is not 201")
         r_annotation3 = requests.get(annotation_url + "&action=download", auth=(user, pw))
+        if r_annotation3.status_code != 200:
+            print("error: r_annotation3 status is not 200")
+        with zipfile.ZipFile(io.BytesIO(r_annotation3.content)) as thezip:
+            for zipinfo in thezip.infolist():
+                with thezip.open(zipinfo) as thefile:
+                    # do something with  zipinfo.filename, thefile
