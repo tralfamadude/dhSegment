7fce851f952046f16677a64aa3d932649c294776
Paul Baclace <peb@pebmac4.localdomain>
2021-03-04 19:38:39 -0800

    started unpacking coco json

--- ia_fetch_annotations.py
+++ ia_fetch_annotations.py
@@ -8,10 +8,12 @@ import zipfile
 import io
 import os
 from glob import glob
-from pycocotools import coco as coco_loader
 
 user = "peb"
 pw = '******'
+
+expected_labels_i2name = {1: 'title_article', 2: 'authors_article', 3: 'refs_article', 4: 'toc'}
+expected_labels_name2i = {'title_article': 1, 'authors_article':2, 'refs_article': 3, 'toc': 4}
 
 r = requests.get('http://localhost:8080/api/v1/projects', auth=("peb", pw))
 if r.status_code != 200:
@@ -19,6 +21,7 @@ if r.status_code != 200:
 r.encoding = 'utf-8'
 project_dict = r.json()
 project_list = project_dict["results"]
+annotation_json = ""
 for project in project_list:
     project_name = project["name"]
     task_list = project["tasks"]
@@ -35,18 +38,39 @@ for project in project_list:
         r_annotation = requests.get(annotation_url, auth=(user, pw))
         if r_annotation.status_code != 202:
             print("error: r_annotation status is not 202")
+            continue
         r_annotation2 = requests.get(annotation_url, auth=(user, pw))
         if r_annotation2.status_code != 201:
             print("error: r_annotation2 status is not 201")
+            continue
         r_annotation3 = requests.get(annotation_url + "&action=download", auth=(user, pw))
         if r_annotation3.status_code != 200:
             print("error: r_annotation3 status is not 200")
+            continue
         with tempfile.TemporaryDirectory() as tmp_dir:
             zipfile.ZipFile(io.BytesIO(r_annotation3.content)).extractall(tmp_dir)
             jsons = glob(os.path.join(tmp_dir, '**', '*.json'), recursive=True)
-            for json in jsons:
-                coco = coco_loader.COCO(json)
-                print(f"FOUND:  {coco.getAnnIds()}")
+            for json_file in jsons:
+                with open(json_file, 'r') as f:
+                    annotation_json = json.load(f)
+                    label_list = annotation_json["categories"]
+                    #
+                    # validate labels (categories in the coco format)
+                    found_labels_i2name = {}
+                    found_labels_name2i = {}
+                    for item in label_list:
+                        found_labels_i2name[item["id"]] = item["name"]
+                        found_labels_name2i[item["name"]] = item["id"]
+                    if expected_labels_i2name != found_labels_i2name:
+                        print(f" task {task_name} has unexpected category/labels, skipping")
+                        continue
+                    if expected_labels_name2i != found_labels_name2i:
+                        print(f" task {task_name} has unexpected category/labels, skipping")
+                        continue
+                    image_list = annotation_json["images"]
+                    annotation_list = annotation_json["annotations"]
+                    
+
 
 
 
