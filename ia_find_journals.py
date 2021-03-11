7e2b284b2a33501b6067195b79f1ecd0f0b78f76
Paul Baclace <peb@pebmac4.localdomain>
2021-03-11 11:04:38 -0800

    debugged, working now

--- ia_find_journals.py
+++ ia_find_journals.py
@@ -1,18 +1,24 @@
 from internetarchive import get_session
 from internetarchive import download
 from datetime import datetime
+import argparse
+import sys
+
 
 class FindJournals:
+    """
+    This simplifies finding journal issue IDs.
+    """
+
     def __init__(self, publication_id: str):
         self.publication_id = publication_id
 
     def iso8601_to_datetime(self, date_string) -> datetime:
         # '2008-01-01T00:00:00Z'
-        date_string = date_string.replace("Z", "+00:00")
-        date = datetime.fromisoformat(date_string)
+        date = datetime.datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
         return date
 
-    def find(self, begin_datetime=None, end_datetime=None) -> list:
+    def find(self, begin_datetime: str = None, end_datetime: str = None) -> list:
         """
         search for issues identifiers. If time range not specified, then all issues found will be returned.
         :param begin_datetime: find content at or after this datetime.
@@ -31,15 +37,53 @@ class FindJournals:
             if begin_datetime is None or end_datetime is None:
                 results.append(item['identifier'])
             else:
-                ds = item['date']
+                ds = item['date']  # example: "2010-01-01T00:00:00Z"
                 dtime = self.iso8601_to_datetime(ds)
                 if dtime >= begin_datetime and dtime < end_datetime:
                     results.append(item['identifier'])
         return results
 
-if __name__ == '__main__':
-    publication_id = 'pub_journal-of-thought'
-    find_journals = FindJournals(publication_id)
-    issues = find_journals()
 
+if __name__ == '__main__':
+    # Example usage:
+    # publication_id = 'pub_journal-of-thought'
+    # find_journals = FindJournals(publication_id)
+    # issues = find_journals.find()
+
+    FLAGS = None
+    # init the parser
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--journal', '-j',
+        type=str,
+        help='journal id, example: pub_journal-of-thought'
+    )
+    parser.add_argument(
+        '--after', '-a',
+        type=str,
+        default=None,
+        help='optional iso date for picking issues at or after, example: 1970-01-31'
+    )
+    parser.add_argument(
+        '--before', '-b',
+        type=str,
+        default=None,
+        help='optional iso date for picking issues before, example: 2010-01-31'
+    )
+    FLAGS, unparsed = parser.parse_known_args()
+    if len(unparsed) > 0:
+        print(f"  Unknown args: {unparsed}")
+        sys.exit(1)
+    journal_id = FLAGS.journal
+    after = FLAGS.after
+    before = FLAGS.before
+    if journal_id is None:
+        parser.print_help()
+        sys.exit(1)
+    find_journals = FindJournals(journal_id)
+    issues = find_journals.find(after, before)
+    for issue_id in issues:
+        print(issue_id)
+    sys.exit(0)
 
